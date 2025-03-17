5ed1c16d4def22f7af01e6cda26d22b13a4ede06
GuiMcL1990 <guirvasco@gmail.com>
2025-03-17 13:55:07 -0300

    Update Application.java

--- Ativ7/Application.java
+++ Ativ7/Application.java
@@ -8,10 +8,9 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Carregar o arquivo FXML
+
         VBox root = FXMLLoader.load(getClass().getResource("verificador_palindromo.fxml"));
 
-        // Criar a cena e configurar o palco
         Scene scene = new Scene(root, 300, 150);
         primaryStage.setTitle("Verificador de Palíndromo");
         primaryStage.setScene(scene);
